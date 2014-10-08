bf9df1f9b076d05ab8487cbbdb120923931aecf1
Christoph Portmann <c.portmann@salamworld.com>
2014-10-08 22:24:09 +0300

    add tail-recursive version

--- permutations.go
+++ permutations.go
@@ -1,15 +1,15 @@
 package main
 
+func merge(ins []rune, c rune) (result []string) {
+	for i := 0; i <= len(ins); i++ {
+		result = append(result, string(ins[:i])+string(c)+string(ins[i:]))
+	}
+	return
+}
+
 func permutations(input string) []string {
 	if len(input) == 1 {
 		return []string{input}
-	}
-
-	var merge = func(ins []rune, c rune) (result []string) {
-		for i := 0; i <= len(ins); i++ {
-			result = append(result, string(ins[:i])+string(c)+string(ins[i:]))
-		}
-		return
 	}
 
 	runes := []rune(input)
@@ -21,4 +21,23 @@ func permutations(input string) []string {
 	}
 
 	return result
+}
+
+
+func permutationsTailRecursive(input string) []string {
+	var helper func(input []rune, previousResult []string) []string
+	helper = func(input []rune, previousResult []string) []string{
+		if len(input) == 0 {
+			return previousResult
+		}else {
+			newResult := []string{}
+			for _, element := range previousResult {
+				newResult = append(newResult, merge([]rune(element), input[0])...)
+			}
+			return helper(input[1:], newResult)
+		}
+	}
+
+	runes := []rune(input)
+	return helper(runes[1:], []string{string(runes[0])})
 }
--- permutations_test.go
+++ permutations_test.go
@@ -6,41 +6,87 @@ import (
 )
 
 func TestPermutations(t *testing.T) {
-	Convey("Test Permutations with one char, should contain only character itself", t, func() {
-			oneCharString := "a"
-			result := permutations(oneCharString)
-			So(len(result), ShouldEqual, 1)
-			So(result[0], ShouldEqual, "a")
-		})
-	Convey("Test Permutations with two chars, should contain original and swapped version", t, func() {
-			twoCharString := "ab"
-			result := permutations(twoCharString)
-			So(len(result), ShouldEqual, 2)
-			So(result, ShouldContain, "ab")
-			So(result, ShouldContain, "ba")
-		})
-	Convey("Test Permutations with three chars, should contain all 6 combinations", t, func() {
-			threeCharString := "abc"
-			result := permutations(threeCharString)
-			So(len(result), ShouldEqual, 6)
-			So(result, ShouldContain, "abc")
-			So(result, ShouldContain, "acb")
-			So(result, ShouldContain, "cab")
-			So(result, ShouldContain, "cba")
-			So(result, ShouldContain, "bac")
-			So(result, ShouldContain, "bca")
-		})
-	Convey("Test Permutations with four, count of permutations should be equals factorial of length of inputstring", t, func() {
-			fourCharString := "abcd"
-			result := permutations(fourCharString)
-			So(len(result), ShouldEqual, 24)
+	Convey("Subject: Test generation of permutations of string, tail recursive implementation", t, func() {
 
-			fiveCharString := "abcde"
-			result = permutations(fiveCharString)
-			So(len(result), ShouldEqual, 120)
+			Convey("Test Permutations with one char, should contain only character itself", func() {
+					oneCharString := "a"
+					result := permutations(oneCharString)
+					So(len(result), ShouldEqual, 1)
+					So(result[0], ShouldEqual, "a")
+				})
+			Convey("Test Permutations with two chars, should contain original and swapped version", func() {
+					twoCharString := "ab"
+					result := permutations(twoCharString)
+					So(len(result), ShouldEqual, 2)
+					So(result, ShouldContain, "ab")
+					So(result, ShouldContain, "ba")
+				})
+			Convey("Test Permutations with three chars, should contain all 6 combinations", func() {
+					threeCharString := "abc"
+					result := permutations(threeCharString)
+					So(len(result), ShouldEqual, 6)
+					So(result, ShouldContain, "abc")
+					So(result, ShouldContain, "acb")
+					So(result, ShouldContain, "cab")
+					So(result, ShouldContain, "cba")
+					So(result, ShouldContain, "bac")
+					So(result, ShouldContain, "bca")
+				})
+			Convey("Test Permutations with four, count of permutations should be equals factorial of length of inputstring", func() {
+					fourCharString := "abcd"
+					result := permutations(fourCharString)
+					So(len(result), ShouldEqual, 24)
 
-			sixCharString := "abcdef"
-			result = permutations(sixCharString)
-			So(len(result), ShouldEqual, 720)
+					fiveCharString := "abcde"
+					result = permutations(fiveCharString)
+					So(len(result), ShouldEqual, 120)
+
+					sixCharString := "abcdef"
+					result = permutations(sixCharString)
+					So(len(result), ShouldEqual, 720)
+				})
+		})
+}
+
+func TestPermutationsTailRecursive(t *testing.T) {
+	Convey("Subject: Test generation of permutations of string, tail recursive implementation", t, func() {
+
+			Convey("Test Permutations with one char, should contain only character itself", func() {
+					oneCharString := "a"
+					result := permutationsTailRecursive(oneCharString)
+					So(len(result), ShouldEqual, 1)
+					So(result[0], ShouldEqual, "a")
+				})
+			Convey("Test Permutations with two chars, should contain original and swapped version", func() {
+					twoCharString := "ab"
+					result := permutationsTailRecursive(twoCharString)
+					So(len(result), ShouldEqual, 2)
+					So(result, ShouldContain, "ab")
+					So(result, ShouldContain, "ba")
+				})
+			Convey("Test Permutations with three chars, should contain all 6 combinations", func() {
+					threeCharString := "abc"
+					result := permutationsTailRecursive(threeCharString)
+					So(len(result), ShouldEqual, 6)
+					So(result, ShouldContain, "abc")
+					So(result, ShouldContain, "acb")
+					So(result, ShouldContain, "cab")
+					So(result, ShouldContain, "cba")
+					So(result, ShouldContain, "bac")
+					So(result, ShouldContain, "bca")
+				})
+			Convey("Test Permutations with four, count of permutations should be equals factorial of length of inputstring", func() {
+					fourCharString := "abcd"
+					result := permutationsTailRecursive(fourCharString)
+					So(len(result), ShouldEqual, 24)
+
+					fiveCharString := "abcde"
+					result = permutations(fiveCharString)
+					So(len(result), ShouldEqual, 120)
+
+					sixCharString := "abcdef"
+					result = permutations(sixCharString)
+					So(len(result), ShouldEqual, 720)
+				})
 		})
 }
